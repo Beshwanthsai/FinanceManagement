98929f6414547e8ecd579aa565afe7af03a73e29
2300030223_GOURISHETTI RUTHVIK <gourishettiruthvik@gmail.com>
2024-10-01 11:20:41 +0530

    models ands urls

--- pythonProject/Financemanagement/FinanceManagementSystem/UserApp/views.py
+++ pythonProject/Financemanagement/FinanceManagementSystem/UserApp/views.py
@@ -9,3 +9,22 @@ def balancecheckpagecall(request):
 
 def addexpensepagecall(request):
     return render(request,'UserApp/AddExpense.html')
+
+# views.py
+from django.shortcuts import render, redirect
+from .models import Expense
+from .forms import ExpenseForm
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('UserApp:ExpenceList')
+    else:
+        form = ExpenseForm()
+    return render(request, 'UserApp/AddExpense.html', {'form': form})
+
+def expense_list(request):
+    expenses = Expense.objects.all()
+    return render(request, 'UserApp/ExpenceList.html', {'expenses': expenses})
