71c770ca430ec50835991f123c5eb315b54ec4a1
2300030223_GOURISHETTI RUTHVIK <gourishettiruthvik@gmail.com>
2024-10-01 11:19:54 +0530

    models ands urls

--- pythonProject/Financemanagement/FinanceManagementSystem/UserApp/models.py
+++ pythonProject/Financemanagement/FinanceManagementSystem/UserApp/models.py
@@ -1,3 +1,18 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    @staticmethod
+    def create_random_categories():
+        random_categories = ['Groceries', 'Travel', 'Education', 'Fitness', 'Gadgets']
+        for category_name in random_categories:
+            if not Category.objects.filter(name=category_name).exists():
+                Category.objects.create(name=category_name)
+
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.TextField()
