a3b695e3d61b2dd98f8b33683fc34c118e40ce8e
Beshwanthsai <2300030312@kluniversity.in>
2024-12-07 15:25:07 +0530

    imported sum

--- pythonProject/Financemanagement/FinanceManagementSystem/UserApp/views.py
+++ pythonProject/Financemanagement/FinanceManagementSystem/UserApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Expense, UserBalance  # Corrected import
 from .forms import ExpenseForm, BalanceForm
+from django.db.models import Sum
 
 def userhomepagecall(request):
     return render(request, 'UserApp/Userapphomepage.html')
